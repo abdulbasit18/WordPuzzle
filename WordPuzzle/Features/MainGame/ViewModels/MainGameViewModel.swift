9703d3d979fce493459c337c9a09be6a4e13d698
Abdul Basit <abdul.basit@veripark.com.pk>
2020-08-23 22:14:21 +0500

    Update MainGameViewModel.swift
    Attach Game logic to ViewModel

--- WordPuzzle/Features/MainGame/ViewModels/MainGameViewModel.swift
+++ WordPuzzle/Features/MainGame/ViewModels/MainGameViewModel.swift
@@ -6,21 +6,162 @@
 //  Copyright © 2020 Abdul Basit. All rights reserved.
 //
 
+import Foundation
 import RxSwift
 
+// MARK: - Input Protocols
+protocol MainGameViewModelInput: class {
+    var viewDidLoadSubject: PublishSubject<Void?> { get }
+    var tappedCorrectAnswerSubject: PublishSubject<Void?> { get }
+    var tappedWrongAnswerSubject: PublishSubject<Void?> { get }
+    var restartSubject: PublishSubject<Void?> { get }
+}
+
+// MARK: - Output Protocols
+protocol MainGameViewModelOutput: class {
+    var displayError: PublishSubject<Error> { get }
+    var displayRoundNumber: PublishSubject<Int> { get }
+    var displayRound: PublishSubject<RoundModel> { get }
+    var display: PublishSubject<GameResults> { get }
+    var gameFinished: PublishSubject<Void?> { get}
+    var removeLoadingAnimation: PublishSubject<Void?> { get }
+    var showLoadingAnimation: PublishSubject<Void?> { get }
+    var speedOfGame: Double { get }
+    var resultSubject: PublishSubject<String> { get }
+}
+
 // MARK: - MainGame Protocol
-protocol MainGameViewModelType {
+protocol MainGameViewModelType: MainGameViewModelInput, MainGameViewModelOutput {
+    var input: MainGameViewModelInput { get }
+    var output: MainGameViewModelOutput { get }
 }
 
 // MARK: - MainGameViewModel Implementation
 
 final class MainGameViewModel: MainGameViewModelType {
+    
+    var input: MainGameViewModelInput { self }
+    var output: MainGameViewModelOutput { self }
+    
+    // MARK: - Inputs
+    var viewDidLoadSubject = PublishSubject<Void?>()
+    var tappedCorrectAnswerSubject = PublishSubject<Void?>()
+    var tappedWrongAnswerSubject = PublishSubject<Void?>()
+    var restartSubject = PublishSubject<Void?>()
+    
+    // MARK: - Outputs
+    var displayError = PublishSubject<Error>()
+    var displayRoundNumber = PublishSubject<Int>()
+    var displayRound = PublishSubject<RoundModel>()
+    var display = PublishSubject<GameResults>()
+    var gameFinished = PublishSubject<Void?>()
+    var removeLoadingAnimation = PublishSubject<Void?>()
+    var showLoadingAnimation = PublishSubject<Void?>()
+    var resultSubject = PublishSubject<String>()
+    var speedOfGame: Double = 5
+    
     // MARK: - Properties
     private let repository: MainGameRepositoryType
+    private var gameCenter: MainGameCenterType
+    let timer: GameTimerType? = AppDelegate.container.resolve(GameTimerType.self)
     private let disposeBag = DisposeBag()
+    private var currentWord: RoundModel?
     
     // MARK: - Initializer
-    init(repository: MainGameRepositoryType) {
+    init(repository: MainGameRepositoryType,
+         gameCenter: MainGameCenterType) {
         self.repository = repository
+        self.gameCenter = gameCenter
+        //Setup RX Bindings
+        setupBindings()
+    }
+    
+    private func setupBindings() {
+        
+        //Inputs
+        input.viewDidLoadSubject
+            .bind(to: repository.input.getWordsSubject)
+            .disposed(by: disposeBag)
+        
+        input.restartSubject.subscribe(onNext: { [weak self] (_) in
+            guard let self = self else { return }
+            self.repository.input.getWordsSubject.onNext(nil)
+        }).disposed(by: disposeBag)
+        
+        input.tappedCorrectAnswerSubject
+            .map { [weak self] (_) -> GameResults.Answer in
+                (self?.currentWord?.isCorrectPair ?? false) ? .correct : .incorrect }
+            .bind(to: gameCenter.updateResultSubject)
+            .disposed(by: disposeBag)
+        
+        input.tappedWrongAnswerSubject
+            .map { [weak self] (_) -> GameResults.Answer in
+                (self?.currentWord?.isCorrectPair ?? false) ? .incorrect : .correct }
+            .bind(to: gameCenter.updateResultSubject)
+            .disposed(by: disposeBag)
+        
+        //Outputs
+        repository.output.wordsDataSubject.subscribe(onNext: { [weak self] (words) in
+            self?.present(words: words)
+        }).disposed(by: disposeBag)
+        
+        gameCenter.outputs.onEventSubject.subscribe(onNext: { (event) in
+            self.stopTimer()
+            switch event {
+                case .newRound(let number):
+                    print("Round Number \(number)")
+                case .newWordPair(let wordPair):
+                    self.currentWord = wordPair
+                    self.output.displayRound.onNext(wordPair)
+                case .currentGameResult(let results):
+                    print("Result \(results)")
+                case .gameFinished(let results):
+                    print("Game Finished")
+                    print(results)
+                    self.prepare(result: results)
+            }
+        }).disposed(by: disposeBag)
+    }
+    
+    private func present(words: [WordModel]) {
+        stopTimer()
+        output.removeLoadingAnimation.onNext(nil)
+        let gameSetting =  GameSettings(numberOfRounds: 1, maximumLimitForRounds: 10, minimumLimitForRounds: 1)
+        setupGameCenter(with: words, gameSettings: gameSetting)
+        startNewRound()
+        startTimer()
+    }
+    
+    private func setupGameCenter(with words: [WordModel], gameSettings: GameSettings) {
+        gameCenter.gameSettings = gameSettings
+        gameCenter.wordPairGenerator.inputs.updateWordsSubject.onNext(words)
+    }
+    
+    private func stopTimer() {
+        timer?.stop()
+    }
+    
+    private func startTimer() {
+        setTimerAction { [weak self] in
+            guard let self = self else { return}
+            self.gameCenter.inputs.updateResultSubject.onNext(.noAnswerProvided)
+            //            self.startNewRound()
+        }
+        timer?.start(timeInterval: speedOfGame)
+    }
+    
+    private func setTimerAction(action: @escaping () -> Void) {
+        timer?.timerAction(action: action)
+    }
+    
+    private func startNewRound() {
+        gameCenter.inputs.startNewRoundSubject.onNext(nil)
+    }
+    
+    private func prepare(result: GameResults) {
+        var resultString = "Correct Answers: \(result.numberOfCorrectAnswers) \n"
+        resultString.append("Wrong Answers: \(result.numberOfIncorrectAnswers) \n")
+        resultString.append("No Answers: \(result.numberOfNoAnswers)")
+        output.resultSubject.onNext(resultString)
     }
 }
